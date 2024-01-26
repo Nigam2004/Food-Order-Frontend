2cc3a63d28c8a3823c06656fecc95a85c915ed00
deepakkumarsahu2 <sahudeepakbca@gmail.com>
2024-01-26 11:37:37 +0530

    deepak changed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div>
-      <h1 className="text-[#f9903e] text-3xl text-center">Nigam</h1>
+      <h1 className="text-[#f9903e] text-3xl text-center">Nigam and deepak</h1>
     </div>
   );
 }
