047d420bede3e209007f9d8ee2fb8a9dd470a3a9
Nigam Bisoyi <nigambisoyi2004@gmail.com>
2024-01-26 11:23:03 +0530

    nigam branch added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div>
-      <h1 className="text-[#f9903e] text-3xl text-center">Swiggy clone</h1>
+      <h1 className="text-[#f9903e] text-3xl text-center">Nigam</h1>
     </div>
   );
 }
