3f8a178182ff6376c2073b17bab4dd2f33d4ccbc
deepakkumarsahu2 <sahudeepakbca@gmail.com>
2024-01-26 11:12:28 +0530

    new PR

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div>
-      <h1 className="text-fuchsia-600">food Order Application</h1>
+      <h1 className="text-[#f9903e] text-3xl text-center">Swiggy clone</h1>
     </div>
   );
 }
