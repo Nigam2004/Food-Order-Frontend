fc51e5a6f757b01fe8ddcd1bd6302384f7a7e08c
Nigam Bisoyi <nigambisoyi2004@gmail.com>
2024-01-27 14:57:12 +0530

    componet separated done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Home from "./pages/home";
-import SliderPage from "./pages/SliderPage";
+
 import Chef from "./pages/Chef";
 function App() {
   return (
     <>
       <Home />
-      <SliderPage />
-      <Chef />
     </>
   );
 }
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,9 +1,8 @@
-import SliderApi from "./SliderApi";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-export default function Carousel() {
+export default function Carousel({ children }) {
   var settings = {
     dots: true,
     infinite: true,
@@ -46,32 +45,7 @@ export default function Carousel() {
   return (
     <>
       <div className="mt-10 ">
-        <Slider {...settings}>
-          {SliderApi.map((da, index) => {
-            return (
-              <div
-                className="bg-white text-black rounded-xl cursor-pointer hover:scale-105  ease-in-out hover:shadow-xl"
-                key={index}
-              >
-                <div className="h-56  bg-[#eee8d7] rounded-lg flex justify-center items-center  ">
-                  <img
-                    src={da.img}
-                    alt={da.name}
-                    className="h-44 w-44 rounded-full"
-                  />
-                </div>
-                <div className="flex flex-col justify-center items-center gap-5 p-6">
-                  <p className="text-xs text-[#fdc868] font-bold font-poppins md:text-xl lg:text-2xl">
-                    {da.name}
-                  </p>
-                  <p className="text-xs font-medium text-center font-poppins md:text-base lg:text-lg">
-                    {da.review}{" "}
-                  </p>
-                </div>
-              </div>
-            );
-          })}
-        </Slider>
+        <Slider {...settings}>{children}</Slider>
       </div>
       ;
     </>
