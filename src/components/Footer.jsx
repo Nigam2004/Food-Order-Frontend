e4f63716415e0fb5b53e59894f14c55736645d3f
deepakkumarsahu2 <sahudeepakbca@gmail.com>
2024-01-27 20:27:54 +0530

    Footer Updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,37 +3,37 @@ import React from "react";
 export default function Footer() {
   return (
     <>
-      <footer class="bg-white font-poppins ">
-        <div class="mx-auto max-w-screen-xl space-y-8 px-4 py-16 sm:px-6 lg:space-y-16 lg:px-8 ">
-          <div class="grid grid-cols-1 gap-8 lg:grid-cols-3">
+      <footer className="bg-white font-poppins ">
+        <div className="mx-auto max-w-screen-xl space-y-8 px-4 py-16 sm:px-6 lg:space-y-16 lg:px-8 ">
+          <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
             <div>
-              <div class="text-teal-600 font-bold">Broccoli Resturant</div>
+              <div className="text-teal-600 font-bold">Broccoli Resturant</div>
 
-              <p class="mt-4 max-w-xs text-gray-500">
+              <p className="mt-4 max-w-xs text-gray-500">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse
                 non cupiditate quae nam molestias.
               </p>
 
-              <ul class="mt-8 flex gap-6 ">
+              <ul className="mt-8 flex gap-6 ">
                 <li>
                   <a
                     href="/"
                     rel="noreferrer"
                     target="_blank"
-                    class="text-orange-400 transition hover:opacity-75"
+                    className="text-orange-400 transition hover:opacity-75"
                   >
-                    <span class="sr-only">Facebook</span>
+                    <span className="sr-only">Facebook</span>
 
                     <svg
-                      class="h-6 w-6"
+                      className="h-6 w-6"
                       fill="currentColor"
                       viewBox="0 0 24 24"
                       aria-hidden="true"
                     >
                       <path
-                        fill-rule="evenodd"
+                        fillRule="evenodd"
                         d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-                        clip-rule="evenodd"
+                        clipRule="evenodd"
                       />
                     </svg>
                   </a>
@@ -44,20 +44,20 @@ export default function Footer() {
                     href="/"
                     rel="noreferrer"
                     target="_blank"
-                    class="text-orange-400 transition hover:opacity-75"
+                    className="text-orange-400 transition hover:opacity-75"
                   >
-                    <span class="sr-only">Instagram</span>
+                    <span className="sr-only">Instagram</span>
 
                     <svg
-                      class="h-6 w-6"
+                      className="h-6 w-6"
                       fill="currentColor"
                       viewBox="0 0 24 24"
                       aria-hidden="true"
                     >
                       <path
-                        fill-rule="evenodd"
+                        fillRule="evenodd"
                         d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-                        clip-rule="evenodd"
+                        clipRule="evenodd"
                       />
                     </svg>
                   </a>
@@ -68,12 +68,12 @@ export default function Footer() {
                     href="/"
                     rel="noreferrer"
                     target="_blank"
-                    class="text-orange-400 transition hover:opacity-75"
+                    className="text-orange-400 transition hover:opacity-75"
                   >
-                    <span class="sr-only">Twitter</span>
+                    <span className="sr-only">Twitter</span>
 
                     <svg
-                      class="h-6 w-6"
+                      className="h-6 w-6"
                       fill="currentColor"
                       viewBox="0 0 24 24"
                       aria-hidden="true"
@@ -88,20 +88,20 @@ export default function Footer() {
                     href="/"
                     rel="noreferrer"
                     target="_blank"
-                    class="text-orange-400 transition hover:opacity-75"
+                    className="text-orange-400 transition hover:opacity-75"
                   >
-                    <span class="sr-only">GitHub</span>
+                    <span className="sr-only">GitHub</span>
 
                     <svg
-                      class="h-6 w-6"
+                      className="h-6 w-6"
                       fill="currentColor"
                       viewBox="0 0 24 24"
                       aria-hidden="true"
                     >
                       <path
-                        fill-rule="evenodd"
+                        fillRule="evenodd"
                         d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                        clip-rule="evenodd"
+                        clipRule="evenodd"
                       />
                     </svg>
                   </a>
@@ -109,15 +109,15 @@ export default function Footer() {
               </ul>
             </div>
 
-            <div class="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:col-span-2 lg:grid-cols-4">
+            <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:col-span-2 lg:grid-cols-4">
               <div>
-                <p class="font-medium text-gray-900">Services</p>
+                <p className="font-medium text-gray-900">Services</p>
 
-                <ul class="mt-6 space-y-4 text-sm">
+                <ul className="mt-6 space-y-4 text-sm">
                   <li>
                     <a
                       href="#"
-                      class="text-orange-400 transition hover:opacity-75"
+                      className="text-orange-400 transition hover:opacity-75"
                     >
                       {" "}
                       Home Delivery{" "}
@@ -127,7 +127,7 @@ export default function Footer() {
                   <li>
                     <a
                       href="#"
-                      class="text-orange-400 transition hover:opacity-75"
+                      className="text-orange-400 transition hover:opacity-75"
                     >
                       {" "}
                       In-Dine Service{" "}
@@ -137,7 +137,7 @@ export default function Footer() {
                   <li>
                     <a
                       href="#"
-                      class="text-orange-400 transition hover:opacity-75"
+                      className="text-orange-400 transition hover:opacity-75"
                     >
                       {" "}
                       Party booking{" "}
@@ -147,7 +147,7 @@ export default function Footer() {
                   <li>
                     <a
                       href="#"
-                      class="text-orange-400 transition hover:opacity-75"
+                      className="text-orange-400 transition hover:opacity-75"
                     >
                       {" "}
                       Club & Bar facility{" "}
@@ -157,13 +157,13 @@ export default function Footer() {
               </div>
 
               <div>
-                <p class="font-medium text-gray-900">Company</p>
+                <p className="font-medium text-gray-900">Company</p>
 
-                <ul class="mt-6 space-y-4 text-sm">
+                <ul className="mt-6 space-y-4 text-sm">
                   <li>
                     <a
                       href="#"
-                      class="text-orange-400 transition hover:opacity-75"
+                      className="text-orange-400 transition hover:opacity-75"
                     >
                       {" "}
                       About{" "}
@@ -173,7 +173,7 @@ export default function Footer() {
                   <li>
                     <a
                       href="#"
-                      class="text-orange-400 transition hover:opacity-75"
+                      className="text-orange-400 transition hover:opacity-75"
                     >
                       {" "}
                       Meet the Team{" "}
@@ -183,7 +183,7 @@ export default function Footer() {
                   <li>
                     <a
                       href="#"
-                      class="text-orange-400 transition hover:opacity-75"
+                      className="text-orange-400 transition hover:opacity-75"
                     >
                       {" "}
                       Accounts Review{" "}
@@ -193,13 +193,13 @@ export default function Footer() {
               </div>
 
               <div>
-                <p class="font-medium text-gray-900">Helpful Links</p>
+                <p className="font-medium text-gray-900">Helpful Links</p>
 
-                <ul class="mt-6 space-y-4 text-sm">
+                <ul className="mt-6 space-y-4 text-sm">
                   <li>
                     <a
                       href="#"
-                      class="text-orange-400 transition hover:opacity-75"
+                      className="text-orange-400 transition hover:opacity-75"
                     >
                       {" "}
                       Contact{" "}
@@ -209,7 +209,7 @@ export default function Footer() {
                   <li>
                     <a
                       href="#"
-                      class="text-orange-400 transition hover:opacity-75"
+                      className="text-orange-400 transition hover:opacity-75"
                     >
                       {" "}
                       FAQs{" "}
@@ -219,7 +219,7 @@ export default function Footer() {
                   <li>
                     <a
                       href="#"
-                      class="text-orange-400 transition hover:opacity-75"
+                      className="text-orange-400 transition hover:opacity-75"
                     >
                       {" "}
                       Live Chat{" "}
@@ -229,13 +229,13 @@ export default function Footer() {
               </div>
 
               <div>
-                <p class="font-medium text-gray-900">Legal</p>
+                <p className="font-medium text-gray-900">Legal</p>
 
-                <ul class="mt-6 space-y-4 text-sm">
+                <ul className="mt-6 space-y-4 text-sm">
                   <li>
                     <a
                       href="#"
-                      class="text-orange-400 transition hover:opacity-75"
+                      className="text-orange-400 transition hover:opacity-75"
                     >
                       {" "}
                       Accessibility{" "}
@@ -245,7 +245,7 @@ export default function Footer() {
                   <li>
                     <a
                       href="#"
-                      class="text-orange-400 transition hover:opacity-75"
+                      className="text-orange-400 transition hover:opacity-75"
                     >
                       {" "}
                       Returns Policy{" "}
@@ -255,7 +255,7 @@ export default function Footer() {
                   <li>
                     <a
                       href="#"
-                      class="text-orange-400 transition hover:opacity-75"
+                      className="text-orange-400 transition hover:opacity-75"
                     >
                       {" "}
                       Refund Policy{" "}
@@ -265,7 +265,7 @@ export default function Footer() {
                   <li>
                     <a
                       href="#"
-                      class="text-orange-400 transition hover:opacity-75"
+                      className="text-orange-400 transition hover:opacity-75"
                     >
                       {" "}
                       Hiring Statistics{" "}
@@ -276,7 +276,7 @@ export default function Footer() {
             </div>
           </div>
 
-          <p class="text-xs text-gray-500">
+          <p className="text-xs text-gray-500">
             &copy; 2024 Broccoli Resturant. All rights reserved.
           </p>
         </div>
